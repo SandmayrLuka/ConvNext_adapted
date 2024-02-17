4cdd8a39f6cc8cb4f3b66a631b9c4b8a276f6e64
Luka Sandmayr <luka-nils.sandmayr@stud.sbg.ac.at>
2024-02-17 11:43:58 +0100

    Revert Downsampling Layers

--- models/convnext.py
+++ models/convnext.py
@@ -78,7 +78,7 @@ class ConvNeXt(nn.Module):
         for i in range(3):
             downsample_layer = nn.Sequential(
                     LayerNorm(dims[i], eps=1e-6, data_format="channels_first"),
-                    nn.Conv2d(dims[i], dims[i+1], kernel_size=2, stride=2),
+                    nn.Conv2d(dims[i], dims[i+1], kernel_size=3, stride=2),
             )
             self.downsample_layers.append(downsample_layer)
 
