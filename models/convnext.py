8930faf3293f248c443f337dca750e8c405b3dfe
Luka Sandmayr <luka-nils.sandmayr@stud.sbg.ac.at>
2024-02-18 09:34:24 +0100

    Changes

--- models/convnext.py
+++ models/convnext.py
@@ -72,12 +72,10 @@ class ConvNeXt(nn.Module):
         self.downsample_layers = nn.ModuleList() # stem and 3 intermediate downsampling conv layers
         stem = nn.Sequential(
             nn.Conv2d(in_chans, dims[0], kernel_size=4, stride=4),
-            LayerNorm(dims[0], eps=1e-6, data_format="channels_first")
         )
         self.downsample_layers.append(stem)
         for i in range(3):
             downsample_layer = nn.Sequential(
-                    LayerNorm(dims[i], eps=1e-6, data_format="channels_first"),
                     nn.Conv2d(dims[i], dims[i+1], kernel_size=3, stride=2),
             )
             self.downsample_layers.append(downsample_layer)
@@ -93,7 +91,6 @@ class ConvNeXt(nn.Module):
             self.stages.append(stage)
             cur += depths[i]
 
-        self.norm = nn.LayerNorm(dims[-1], eps=1e-6) # final norm layer
         self.head = nn.Linear(dims[-1], num_classes)
 
         self.apply(self._init_weights)
